5f5a1e3a2bc3edcb7a49c4a8161f605403702a9c
Jeff <Jpannell6@gatech.edu>
2025-03-23 19:36:47 -0400

    Correcting error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 export default function page() {
     return (
-      <main classNmae="flex flex-col items-center justify-center min-h-screen bg-white text-black">
+      <main className="flex flex-col items-center justify-center min-h-screen bg-white text-black">
         <h1 className="text-2xl font-bold">Group 41 Team Project</h1>
         <p className="mt-4 text-lg">Bryan Lopez, Charles Roberts, Smita Tejo, Jeffrey S. Pannell</p>
         <p className="mt-4 text-med">
-          The goal of this project is to create efficiency for medical providers with doctorpatient interactions
+          The goal of this project is to create efficiency in work-flow for medical providers with doctorpatient interactions
         </p>
         
       </main>
