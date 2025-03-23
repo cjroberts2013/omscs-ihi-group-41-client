2d86e3c368c5ea179cd0ca972823c6d3eb0e19d2
Jeff <Jpannell6@gatech.edu>
2025-03-23 19:03:09 -0400

    Test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 export default function page() {
     return (
-      <div>
-      Home
-      </div>
+      <main classNmae="flex flex-col items-center justify-center min-h-screen bg-white text-black">
+        <h1 className="text-2xl font-bold">Group 41 Team Project</h1>
+        <p className="mt-4 text-lg">Bryan Lopez, Charles Roberts, Smita Tejo, Jeffrey S. Pannell</p>
+        <p className="mt-4 text-med">
+          The goal of this project is to create efficiency for medical providers with doctorpatient interactions
+        </p>
+        
+      </main>
+     
     );
   }
