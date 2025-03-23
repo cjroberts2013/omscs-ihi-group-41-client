436f823530e475af696ed0ec719f7f13179e0484
Charles Roberts <cjroberts2013@gmail.com>
2025-03-23 11:43:23 -0500

    Small fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,137 +45,138 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-const [sidebarOpen, setSidebarOpen] = useState(false)
-const [currentMenu, setCurrentMenu] = useState("Home")
+    const [sidebarOpen, setSidebarOpen] = useState(false)
+    const [currentMenu, setCurrentMenu] = useState("Home")
 
-return (
-    <html className="h-full bg-white">
-    <body className="h-full">
-    <>
-        {/*
-        This example requires updating your template:
+    return (
+        <html className="h-full bg-white">
+            <body className="h-full">
+                <>
+                    {/*
+                    This example requires updating your template:
 
-        ```
-        <html class="h-full bg-white">
-        <body class="h-full">
-        ```
-      */}
-        <div>
-            <Dialog open={sidebarOpen} onClose={setSidebarOpen} className="relative z-50 lg:hidden">
-                <DialogBackdrop
-                    transition
-                    className="fixed inset-0 bg-gray-900/80 transition-opacity duration-300 ease-linear data-[closed]:opacity-0"
-                />
+                    ```
+                    <html class="h-full bg-white">
+                    <body class="h-full">
+                    ```
+                  */}
+                    <div>
+                        <Dialog open={sidebarOpen} onClose={setSidebarOpen} className="relative z-50 lg:hidden">
+                            <DialogBackdrop
+                                transition
+                                className="fixed inset-0 bg-gray-900/80 transition-opacity duration-300 ease-linear data-[closed]:opacity-0"
+                            />
 
-                <div className="fixed inset-0 flex">
-                    <DialogPanel
-                        transition
-                        className="relative mr-16 flex w-full max-w-xs flex-1 transform transition duration-300 ease-in-out data-[closed]:-translate-x-full"
-                    >
-                        <TransitionChild>
-                            <div
-                                className="absolute left-full top-0 flex w-16 justify-center pt-5 duration-300 ease-in-out data-[closed]:opacity-0">
-                                <button type="button" onClick={() => setSidebarOpen(false)} className="-m-2.5 p-2.5">
-                                    <span className="sr-only">Close sidebar</span>
-                                    <XMarkIcon aria-hidden="true" className="size-6 text-white"/>
-                                </button>
-                            </div>
-                        </TransitionChild>
-                        {/* Sidebar component, swap this element with another sidebar if you like */}
-                        <div
-                            className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-2 ring-1 ring-white/10">
-                            <div className="flex h-16 shrink-0 items-center">
-                                {/*TODO: Add GT logo here*/}
-                                <Image
-                                    alt="Your Company"
-                                    src={GTLogo}
-                                    className="h-8 w-auto"
-                                />
-                            </div>
-                            <nav className="flex flex-1 flex-col">
-                                <ul role="list" className="flex flex-1 flex-col gap-y-7">
-                                    <li>
-                                        <ul role="list" className="-mx-2 space-y-1">
-                                            {navigation.map((item) => (
-                                                <li key={item.name}>
-                                                    <Link
-                                                        onClick={() => {setCurrentMenu(item.name)}}
-                                                        href={item.href}
-                                                        className={classNames(
-                                                            item.name == currentMenu
-                                                                ? 'bg-gray-800 text-white'
-                                                                : 'text-gray-400 hover:bg-gray-800 hover:text-white',
-                                                            'group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold',
-                                                        )}
-                                                    >
-                                                        <item.icon aria-hidden="true" className="size-6 shrink-0"/>
-                                                        {item.name}
-                                                    </Link>
+                            <div className="fixed inset-0 flex">
+                                <DialogPanel
+                                    transition
+                                    className="relative mr-16 flex w-full max-w-xs flex-1 transform transition duration-300 ease-in-out data-[closed]:-translate-x-full"
+                                >
+                                    <TransitionChild>
+                                        <div
+                                            className="absolute left-full top-0 flex w-16 justify-center pt-5 duration-300 ease-in-out data-[closed]:opacity-0">
+                                            <button type="button" onClick={() => setSidebarOpen(false)} className="-m-2.5 p-2.5">
+                                                <span className="sr-only">Close sidebar</span>
+                                                <XMarkIcon aria-hidden="true" className="size-6 text-white"/>
+                                            </button>
+                                        </div>
+                                    </TransitionChild>
+                                    {/* Sidebar component, swap this element with another sidebar if you like */}
+                                    <div
+                                        className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-2 ring-1 ring-white/10">
+                                        <div className="flex h-16 shrink-0 items-center">
+                                            {/*TODO: Add GT logo here*/}
+                                            <Image
+                                                alt="Your Company"
+                                                src={GTLogo}
+                                                className="h-8 w-auto"
+                                            />
+                                        </div>
+                                        <nav className="flex flex-1 flex-col">
+                                            <ul role="list" className="flex flex-1 flex-col gap-y-7">
+                                                <li>
+                                                    <ul role="list" className="-mx-2 space-y-1">
+                                                        {navigation.map((item) => (
+                                                            <li key={item.name}>
+                                                                <Link
+                                                                    onClick={() => {setCurrentMenu(item.name)}}
+                                                                    href={item.href}
+                                                                    className={classNames(
+                                                                        item.name == currentMenu
+                                                                            ? 'bg-gray-800 text-white'
+                                                                            : 'text-gray-400 hover:bg-gray-800 hover:text-white',
+                                                                        'group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold',
+                                                                    )}
+                                                                >
+                                                                    <item.icon aria-hidden="true" className="size-6 shrink-0"/>
+                                                                    {item.name}
+                                                                </Link>
+                                                            </li>
+                                                        ))}
+                                                    </ul>
                                                 </li>
-                                            ))}
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </nav>
-                        </div>
-                    </DialogPanel>
-                </div>
-            </Dialog>
+                                            </ul>
+                                        </nav>
+                                    </div>
+                                </DialogPanel>
+                            </div>
+                        </Dialog>
 
-            {/* Static sidebar for desktop */}
-            <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
-                {/* Sidebar component, swap this element with another sidebar if you like */}
-                <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6">
-                    <div className="flex h-16 shrink-0 items-center">
-                        <Image
-                            alt="Your Company"
-                            src={GTLogo}
-                            className="h-8 w-auto"
-                        />
-                    </div>
-                    <nav className="flex flex-1 flex-col">
-                        <ul role="list" className="flex flex-1 flex-col gap-y-7">
-                            <li>
-                                <ul role="list" className="-mx-2 space-y-1">
-                                    {navigation.map((item) => (
-                                        <li key={item.name}>
-                                            <Link
-                                                onClick={() => {setCurrentMenu(item.name)}}
-                                                href={item.href}
-                                                className={classNames(
-                                                    item.name == currentMenu
-                                                        ? 'bg-gray-800 text-white'
-                                                        : 'text-gray-400 hover:bg-gray-800 hover:text-white',
-                                                    'group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold',
-                                                )}
-                                            >
-                                                <item.icon aria-hidden="true" className="size-6 shrink-0"/>
-                                                {item.name}
-                                            </Link>
+                        {/* Static sidebar for desktop */}
+                        <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+                            {/* Sidebar component, swap this element with another sidebar if you like */}
+                            <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6">
+                                <div className="flex h-16 shrink-0 items-center">
+                                    <Image
+                                        alt="Your Company"
+                                        src={GTLogo}
+                                        className="h-8 w-auto"
+                                    />
+                                </div>
+                                <nav className="flex flex-1 flex-col">
+                                    <ul role="list" className="flex flex-1 flex-col gap-y-7">
+                                        <li>
+                                            <ul role="list" className="-mx-2 space-y-1">
+                                                {navigation.map((item) => (
+                                                    <li key={item.name}>
+                                                        <Link
+                                                            onClick={() => {setCurrentMenu(item.name)}}
+                                                            href={item.href}
+                                                            className={classNames(
+                                                                item.name == currentMenu
+                                                                    ? 'bg-gray-800 text-white'
+                                                                    : 'text-gray-400 hover:bg-gray-800 hover:text-white',
+                                                                'group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold',
+                                                            )}
+                                                        >
+                                                            <item.icon aria-hidden="true" className="size-6 shrink-0"/>
+                                                            {item.name}
+                                                        </Link>
+                                                    </li>
+                                                ))}
+                                            </ul>
                                         </li>
-                                    ))}
-                                </ul>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
 
-            <div
-                className="sticky top-0 z-40 flex items-center gap-x-6 bg-gray-900 px-4 py-4 shadow-sm sm:px-6 lg:hidden">
-                <button type="button" onClick={() => setSidebarOpen(true)}
-                        className="-m-2.5 p-2.5 text-gray-400 lg:hidden">
-                    <span className="sr-only">Open sidebar</span>
-                    <Bars3Icon aria-hidden="true" className="size-6"/>
-                </button>
-                <div className="flex-1 text-sm/6 font-semibold text-white">Dashboard</div>
-            </div>
+                        <div
+                            className="sticky top-0 z-40 flex items-center gap-x-6 bg-gray-900 px-4 py-4 shadow-sm sm:px-6 lg:hidden">
+                            <button type="button" onClick={() => setSidebarOpen(true)}
+                                    className="-m-2.5 p-2.5 text-gray-400 lg:hidden">
+                                <span className="sr-only">Open sidebar</span>
+                                <Bars3Icon aria-hidden="true" className="size-6"/>
+                            </button>
+                            <div className="flex-1 text-sm/6 font-semibold text-white">Dashboard</div>
+                        </div>
 
-            <main className="py-10 lg:pl-72">
-                <div className="px-4 sm:px-6 lg:px-8">{children}</div>
-            </main>
-        </div>
-    </>
-    </body>
-    </html>
-)
+                        <main className="py-10 lg:pl-72">
+                            <div className="px-4 sm:px-6 lg:px-8">{children}</div>
+                        </main>
+                    </div>
+                </>
+            </body>
+        </html>
+    );
+}
