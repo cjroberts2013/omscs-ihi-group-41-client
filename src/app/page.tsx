9af804b1b4ed6e3a761594872fac805eb55f3b62
Jeff <Jpannell6@gatech.edu>
2025-03-23 21:14:28 -0400

    i

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function page() {
         <p className="mt-4 text-left text-med">
           The goal of this project is to create efficiency in work-flow for medical providers when interacting with patients. To accomplish this task, our team developed a web-based application, allowing medical providers to record the interaction in the background. Since the patient information is captured in the background the provider is no longer required to focus on a screen as they are interact with the patient. This increased focus on interacting with the patient helps in building a rapport leading to increased trust in the medical provider for the patient.
         </p>
-        <Image src="images/*Doctors-blog480.jpg" alt="Frustrated patient" width={500} height={300} className="w-full h-auto" />
+        <Image src="images/08Doctors-blog480.jpg" alt="Frustrated patient" width={500} height={300} className="w-full h-auto" />
         <p className="mt-4 text-left text-med">
           To record the interaction the provider simply opens the web page, clicks record, the app then records the converstion in the background, submits it to chatGPT, creates structured data the provider can review, then with the simple click of a button the data is uploaded to a FHIR server.
 
