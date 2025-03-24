aa22382ac226bc4ce67bd2b74e749567c18d5280
Jeff <Jpannell6@gatech.edu>
2025-03-23 20:01:14 -0400

    View test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 export default function page() {
     return (
       <main className="flex flex-col items-center justify-center min-h-screen bg-white text-black">
-        <h1 className="text-2xl font-bold">Group 41 Team Project</h1>
-        <p className="mt-4 text-lg">Bryan Lopez, Charles Roberts, Smita Tejo, Jeffrey S. Pannell</p>
-        <p className="mt-4 text-med">
-          The goal of this project is to create efficiency in work-flow for medical providers with doctorpatient interactions
+        <h1 className="text-lg text-left font-bold">Group 41 Team Project</h1>
+        <p className="mt-4 text-left text-med">Bryan Lopez, Charles Roberts, Smita Tejo, Jeffrey S. Pannell</p>
+        <p className="mt-4 text-center text-med">
+          The goal of this project is to create efficiency in work-flow for medical providers with doctor-patient interactions. 
+          To accomplish this task, our team developed a web-based application for medical providers to record the interaction in the background. 
+          Since the patient information is captured in the background the provider is no longer required to focus on a screen as they are interacting with the patient.
         </p>
-        
+        <p className="mt-4 text-center text-med">
+
+
+
+        </p>  
       </main>
      
     );
